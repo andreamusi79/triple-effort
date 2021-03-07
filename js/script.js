27c4777d6be2b8291ffc32f63f9285c764dc79e5
andreamusi79 <andreamusi79@gmail.com>
2021-03-07 10:56:21 +0100

    adding dropdown menu

--- js/script.js
+++ js/script.js
@@ -9,16 +9,30 @@ $('.collapse_link').click(function(){
         }
     });
 
-
-  // Initialize tooltip component
-  $('[data-toggle="tooltip"]').tooltip({
+// Initialize tooltip component
+$('[data-toggle="tooltip"]').tooltip({
     trigger : 'hover'
   })
-  // Prevent the page to scroll back to the top when clicking on the tooltip
-  $('[data-toggle="tooltip"]').on('click', function(e) {e.preventDefault(); return true;});  
+// Prevent the page to scroll back to the top when clicking on the tooltip
+$('[data-toggle="tooltip"]').on('click', function(e) {e.preventDefault(); return true;});
 
+// Allow bootstrap navbar open on first click and go to link on second click
+$('.dropdown').on('show.bs.dropdown', function () {
+    $(this).siblings('.open').removeClass('open').find('a.dropdown-toggle').attr('data-toggle', 'dropdown');
+    $(this).find('a.dropdown-toggle').removeAttr('data-toggle');
+  });
 
-
-
-
+// Hide the dropdown menu when the user click outside of the trigger element
+// $(document).ready(function(){
+//     // Show hide popover
+//     $(".dropdown").click(function(){
+//         $(this).find(".dropdown-menu").slideToggle("fast");
+//     });
+// });
+$(document).on("click", function(event){
+    var $trigger = $(".dropdown");
+    if($trigger !== event.target && !$trigger.has(event.target).length){
+        $(".dropdown-menu").slideUp("fast");
+    }            
+});
 
